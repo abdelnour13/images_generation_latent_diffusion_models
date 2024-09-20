e91216c794e23a780d5c67e3024d124f6eca87bc
Abdelnour <ab.fellah@esi-sba.dz>
2024-09-20 16:34:58 +0100

    delete gan files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .vqvae import VQVAE,VQVAEConfig
 from .discriminator import Discriminator,DiscriminatorConfig
 from .unet import UNet,UNetConfig
-from .generator import Generator,GeneratorConfig
 from .latent_diffusion import LatentDiffusion,LatentDiffusionConfig
